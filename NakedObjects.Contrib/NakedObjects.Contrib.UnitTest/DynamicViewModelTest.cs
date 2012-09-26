64fa764774272d3d204f5fa7de410e09df1ce1a8
Dan Haywood <dan@haywood-associates.co.uk>
2012-09-26 13:44:52 +0100

    take-on of solution.

---
+++ NakedObjects.Contrib/NakedObjects.Contrib.UnitTest/DynamicViewModelTest.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NakedObjects.Contrib.UnitTest
+{
+    [TestClass]
+    public class DynamicViewModelTest
+    {
+        public TestContext TestContext { get; set; }
+
+        
+        [TestMethod]
+        public void SketchToFigureOutHowToCallAVarargsMethodReflectively()
+        {
+            var methodInfo = typeof (DynamicViewModel).GetMethod("ToList", new Type[] {typeof (string[])});
+            methodInfo.Should().NotBeNull();
+            var invoke = methodInfo.Invoke(null, new object[] {new string[] {"foo", "bar"}});
+            var returnVal = invoke as List<string>;
+            returnVal.Should().NotBeNull();
+            returnVal[0].Should().Be("foo");
+        }
+
+        [TestMethod]
+        public void BuildType()
+        {
+            var propertySpecs = new List<DynamicViewModel.PropertySpec>();
+            var dvm = DynamicViewModel.Create<MyDynamicViewModel>("MyCompany", "MyCompany.MyDynamicViewModel_2", propertySpecs);
+            dvm.GetType().FullName.Should().Be("MyCompany.MyDynamicViewModel_2");
+
+            // there are no additional properties
+            var propertyInfo = dvm.GetType().GetProperty("FirstName");
+            propertyInfo.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void BuildTypeWithProperty()
+        {
+            var propertySpecs = new List<DynamicViewModel.PropertySpec>
+                                    {
+                                        new DynamicViewModel.PropertySpec
+                                            {
+                                                Name = "FirstName",
+                                                Type = typeof(string),
+                                                Value = "Joe"
+                                            }
+                                    };
+            var dvm = DynamicViewModel.Create<MyDynamicViewModel>("MyCompany", "MyCompany.MyDynamicViewModel_1", propertySpecs);
+
+            var propertyInfo = dvm.GetType().GetProperty("FirstName");
+            propertyInfo.Should().NotBeNull();
+            propertyInfo.GetValue(dvm, new object[] { }).Should().Be("Joe");
+        }
+
+        [TestMethod]
+        public void BuildTypeWithPropertyThatHasAnnotations()
+        {
+            var propertySpecs = new List<DynamicViewModel.PropertySpec>
+                                    {
+                                        new DynamicViewModel.PropertySpec
+                                            {
+                                                Name = "Comments",
+                                                Type = typeof (string),
+                                                MemberOrder = 7,
+                                                DisplayName = "Comments here",
+                                                Mandatory = false,
+                                                DescribedAs = "Please enter any comments here",
+                                                TypicalLength = 40,
+                                                MaxLength = 200,
+                                                MultiLineNumberOfLines = 10,
+                                                MultiLineWidth = 20,
+                                            }
+                                    };
+            var dvm = DynamicViewModel.Create<MyDynamicViewModel>("MyCompany", "MyCompany.MyDynamicViewModel_4", propertySpecs);
+
+            var propertyInfo = dvm.GetType().GetProperty("Comments");
+            propertyInfo.Should().NotBeNull();
+
+            var customAttributes = propertyInfo.GetCustomAttributes(false);
+
+            var memberOrderAttribute = GetAttribute<MemberOrderAttribute>(customAttributes);
+            memberOrderAttribute.Should().NotBeNull();
+            memberOrderAttribute.Sequence.Should().Be("7");
+
+            var displayNameAttribute = GetAttribute<DisplayNameAttribute>(customAttributes);
+            displayNameAttribute.Should().NotBeNull();
+            displayNameAttribute.DisplayName.Should().Be("Comments here");
+
+            var optionallyAttribute = GetAttribute<OptionallyAttribute>(customAttributes);
+            optionallyAttribute.Should().NotBeNull();
+
+            var describedAsAttribute = GetAttribute<DescribedAsAttribute>(customAttributes);
+            describedAsAttribute.Should().NotBeNull();
+            describedAsAttribute.Value.Should().Be("Please enter any comments here");
+
+            var typicalLengthAttribute = GetAttribute<TypicalLengthAttribute>(customAttributes);
+            typicalLengthAttribute.Should().NotBeNull();
+            typicalLengthAttribute.Value.Should().Be(40);
+
+            var maxLengthAttribute = GetAttribute<MaxLengthAttribute>(customAttributes);
+            maxLengthAttribute.Should().NotBeNull();
+            maxLengthAttribute.Value.Should().Be(200);
+
+            var multiLineAttribute = GetAttribute<MultiLineAttribute>(customAttributes);
+            multiLineAttribute.Should().NotBeNull();
+            multiLineAttribute.NumberOfLines.Should().Be(10);
+            multiLineAttribute.Width.Should().Be(20);
+        }
+
+        [TestMethod]
+        public void BuildTypeWithPropertyThatMultiLineNumberOfLinesOnly()
+        {
+            var propertySpecs = new List<DynamicViewModel.PropertySpec>
+                                    {
+                                        new DynamicViewModel.PropertySpec
+                                            {
+                                                Name = "Comments",
+                                                Type = typeof (string),
+                                                MultiLineNumberOfLines = 10
+                                            }
+                                    };
+            var dvm = DynamicViewModel.Create<MyDynamicViewModel>("MyCompany", "MyCompany.MyDynamicViewModel_5", propertySpecs);
+
+            var propertyInfo = dvm.GetType().GetProperty("Comments");
+            propertyInfo.Should().NotBeNull();
+
+            var customAttributes = propertyInfo.GetCustomAttributes(false);
+
+            var multiLineAttribute = GetAttribute<MultiLineAttribute>(customAttributes);
+            multiLineAttribute.Should().NotBeNull();
+            multiLineAttribute.NumberOfLines.Should().Be(10);
+            multiLineAttribute.Width.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void BuildTypeWithPropertyThatMultiLineWidthOnly()
+        {
+            var propertySpecs = new List<DynamicViewModel.PropertySpec>
+                                    {
+                                        new DynamicViewModel.PropertySpec
+                                            {
+                                                Name = "Comments",
+                                                Type = typeof (string),
+                                                MultiLineWidth = 20,
+                                            }
+                                    };
+            var dvm = DynamicViewModel.Create<MyDynamicViewModel>("MyCompany", "MyCompany.MyDynamicViewModel_6", propertySpecs);
+
+            var propertyInfo = dvm.GetType().GetProperty("Comments");
+            propertyInfo.Should().NotBeNull();
+
+            var customAttributes = propertyInfo.GetCustomAttributes(false);
+
+            var multiLineAttribute = GetAttribute<MultiLineAttribute>(customAttributes);
+            multiLineAttribute.Should().NotBeNull();
+            multiLineAttribute.Width.Should().Be(20);
+            multiLineAttribute.NumberOfLines.Should().Be(6); // default
+        }
+
+        [TestMethod]
+        public void BuildTypeWithPropertyThatHasChoices()
+        {
+            var propertySpecs = new List<DynamicViewModel.PropertySpec>
+                                    {
+                                        new DynamicViewModel.PropertySpec
+                                            {
+                                                Name = "PaymentMethod",
+                                                Type = typeof (string),
+                                                Choices = new List<string> {"Visa", "Mastercard", "Amex", "PayPal"}
+                                            }
+                                    };
+            var dvm = DynamicViewModel.Create<MyDynamicViewModel>("MyCompany", "MyCompany.MyDynamicViewModel_3", propertySpecs);
+
+            var propertyInfo = dvm.GetType().GetProperty("PaymentMethod");
+            propertyInfo.Should().NotBeNull();
+
+            var methodInfo = dvm.GetType().GetMethod("ChoicesPaymentMethod");
+            methodInfo.Should().NotBeNull();
+
+            var retval = methodInfo.Invoke(dvm, new object[] {}) as List<string>;
+            retval.Should().NotBeNull();
+            retval[0].Should().Be("Visa");
+            retval[1].Should().Be("Mastercard");
+        }
+
+
+        private static T GetAttribute<T>(IEnumerable<object> customAttributes)
+        {
+            foreach (var customAttribute in customAttributes)
+            {
+                if (customAttribute is T)
+                {
+                    return (T)customAttribute;
+                }
+            }
+            return default(T);
+        }
+    }
+
+    public class MyDynamicViewModel : DynamicViewModel {}
+
+
+}
---
+++ NakedObjects.Contrib/NakedObjects.Contrib/DynamicViewModel.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using NakedObjects;
+
+namespace NakedObjects.Contrib
+{
+    [NotPersisted]
+    public abstract class DynamicViewModel
+    {
+        public class PropertySpec
+        {
+            public string Name { get; set; }
+            public Type Type { get; set; }
+            public string DisplayName { get; set; }
+            public double MemberOrder { get; set; }
+            public string DescribedAs { get; set; }
+
+            public bool Mandatory { get; set; }
+            public int? TypicalLength { get; set; }
+            public int? MaxLength { get; set; }
+            public int? MultiLineNumberOfLines { get; set; }
+            public int? MultiLineWidth { get; set; }
+
+            public object Value { get; set; }
+
+            public List<string> Choices { get; set; }
+        }
+
+        private static readonly ConstructorInfo DisplayNameAttribute = typeof(DisplayNameAttribute).GetConstructor(new[] { typeof(string) });
+        private static readonly ConstructorInfo MemberOrderAttribute = typeof(MemberOrderAttribute).GetConstructor(new[] { typeof(double) });
+        private static readonly ConstructorInfo OptionallyAttribute = typeof(OptionallyAttribute).GetConstructor(new Type[0]);
+        private static readonly ConstructorInfo DescribedAsAttribute = typeof(DescribedAsAttribute).GetConstructor(new[] { typeof(string) });
+        private static readonly ConstructorInfo TypicalLengthAttribute = typeof(TypicalLengthAttribute).GetConstructor(new[] { typeof(int) });
+        private static readonly ConstructorInfo MaxLengthAttribute = typeof(MaxLengthAttribute).GetConstructor(new[] { typeof(int) });
+        private static readonly ConstructorInfo MultiLineAttributeCi = typeof(MultiLineAttribute).GetConstructor(new Type[0]);
+        private static readonly PropertyInfo MultiLineAttributePiNumberOfLines = typeof(MultiLineAttribute).GetProperty("NumberOfLines");
+        private static readonly PropertyInfo MultiLineAttributePiWidth = typeof(MultiLineAttribute).GetProperty("Width");
+
+        private static ModuleBuilder ModuleBuilder { get; set; }
+
+        public static T Create<T>(string assemblyName, string typeName, List<PropertySpec> propertySpecs) where T : DynamicViewModel
+        {
+            EnsureModuleBuilderExists(assemblyName);
+
+            var dynamicType = ObtainType<T>(typeName, propertySpecs);
+            var newRep = InstantiateAndInitializeInstance<T>(dynamicType, propertySpecs);
+
+            return (T)newRep;
+        }
+
+        private static object InstantiateAndInitializeInstance<T>(Type dynamicType, IEnumerable<PropertySpec> propertySpecs)
+            where T : DynamicViewModel
+        {
+            var instance = Activator.CreateInstance(dynamicType);
+            foreach (var propertySpec in propertySpecs)
+            {
+                if (propertySpec.Value == null)
+                {
+                    continue;
+                }
+
+                PropertyInfo propertyInfo = null;
+                while (propertyInfo == null)
+                {
+                    // very odd; seems that the first time we ask, get back a null.
+                    propertyInfo = dynamicType.GetProperty(propertySpec.Name);
+                }
+                propertyInfo.SetValue(instance, propertySpec.Value, null);
+            }
+            return instance;
+        }
+
+        private static Type ObtainType<T>(string typeName, List<PropertySpec> propertySpecs) where T : DynamicViewModel
+        {
+            var proxyType = ModuleBuilder.GetType(typeName);
+
+            if (proxyType != null)
+            {
+                return proxyType;
+            }
+            var typeBuilder = ModuleBuilder.DefineType(typeName,
+                                                       TypeAttributes.Public | TypeAttributes.Class |
+                                                       TypeAttributes.Sealed,
+                                                       typeof(T));
+            foreach (var propertySpec in propertySpecs)
+            {
+                AppendProperty(typeBuilder, propertySpec);
+            }
+
+            var dynamicType = typeBuilder.CreateType();
+
+            foreach (var propertySpec in propertySpecs)
+            {
+                PropertyInfo propertyInfo = null;
+                while (propertyInfo == null)
+                {
+                    // very odd; seems that the first time we ask, get back a null.
+                    propertyInfo = dynamicType.GetProperty(propertySpec.Name);
+                }
+            }
+            return dynamicType;
+        }
+
+        private static void EnsureModuleBuilderExists(string assemblyName)
+        {
+            if (ModuleBuilder != null)
+            {
+                return;
+            }
+            var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName(assemblyName), AssemblyBuilderAccess.Run);
+            ModuleBuilder = assemblyBuilder.DefineDynamicModule(assemblyName);
+        }
+
+        private static void AppendProperty(TypeBuilder typeBuilder, PropertySpec propertySpec)
+        {
+            var propertyType = propertySpec.Type;
+            var name = propertySpec.Name;
+
+            var fieldBuilder = typeBuilder.DefineField("_" + name, propertyType, FieldAttributes.Private);
+
+            var propertyBuilder = typeBuilder.DefineProperty(name, PropertyAttributes.None, propertyType, Type.EmptyTypes);
+
+            var getMethodBuilder = typeBuilder.DefineMethod("get_" + name, MethodAttributes.SpecialName | MethodAttributes.HideBySig | MethodAttributes.Public, propertyType, Type.EmptyTypes);
+            var iLGenerator = getMethodBuilder.GetILGenerator();
+            iLGenerator.Emit(OpCodes.Ldarg_0);
+            iLGenerator.Emit(OpCodes.Ldfld, fieldBuilder);
+            iLGenerator.Emit(OpCodes.Ret);
+
+            var setMethodBuilder = typeBuilder.DefineMethod("set_" + name, MethodAttributes.SpecialName | MethodAttributes.HideBySig | MethodAttributes.Public, null, new[] { propertyType });
+
+            iLGenerator = setMethodBuilder.GetILGenerator();
+            iLGenerator.Emit(OpCodes.Ldarg_0);
+            iLGenerator.Emit(OpCodes.Ldarg_1);
+            iLGenerator.Emit(OpCodes.Stfld, fieldBuilder);
+            iLGenerator.Emit(OpCodes.Ret);
+
+            propertyBuilder.SetGetMethod(getMethodBuilder);
+            propertyBuilder.SetSetMethod(setMethodBuilder);
+
+            propertyBuilder.SetCustomAttribute(new CustomAttributeBuilder(DisplayNameAttribute, new object[] { propertySpec.DisplayName }));
+            propertyBuilder.SetCustomAttribute(new CustomAttributeBuilder(MemberOrderAttribute, new object[] { propertySpec.MemberOrder }));
+
+            if (propertySpec.DescribedAs != null)
+            {
+                propertyBuilder.SetCustomAttribute(new CustomAttributeBuilder(DescribedAsAttribute, new object[] { propertySpec.DescribedAs }));    
+            }
+
+            if (!propertySpec.Mandatory)
+            {
+                propertyBuilder.SetCustomAttribute(new CustomAttributeBuilder(OptionallyAttribute, new object[0]));
+            }
+
+            if (propertySpec.Type == typeof(string))
+            {
+                if (propertySpec.TypicalLength != null)
+                {
+                    propertyBuilder.SetCustomAttribute(new CustomAttributeBuilder(TypicalLengthAttribute, new object[] { propertySpec.TypicalLength }));
+                }
+
+                if (propertySpec.MaxLength != null)
+                {
+                    propertyBuilder.SetCustomAttribute(new CustomAttributeBuilder(MaxLengthAttribute, new object[] { propertySpec.MaxLength }));
+                }
+
+                if (propertySpec.MultiLineNumberOfLines != null && propertySpec.MultiLineWidth != null)
+                {
+                    propertyBuilder.SetCustomAttribute(new CustomAttributeBuilder(
+                                                           MultiLineAttributeCi, new object[0], 
+                                                           new []{MultiLineAttributePiNumberOfLines, MultiLineAttributePiWidth}, 
+                                                           new object[]{propertySpec.MultiLineNumberOfLines, propertySpec.MultiLineWidth}));
+                }
+
+                if (propertySpec.MultiLineNumberOfLines != null && propertySpec.MultiLineWidth == null)
+                {
+                    propertyBuilder.SetCustomAttribute(new CustomAttributeBuilder(
+                                                           MultiLineAttributeCi, new object[0],
+                                                           new[] {MultiLineAttributePiNumberOfLines},
+                                                           new object[] {propertySpec.MultiLineNumberOfLines}));
+                }
+
+                if (propertySpec.MultiLineNumberOfLines == null && propertySpec.MultiLineWidth != null)
+                {
+                    propertyBuilder.SetCustomAttribute(new CustomAttributeBuilder(
+                                                           MultiLineAttributeCi, new object[0],
+                                                           new [] { MultiLineAttributePiWidth },
+                                                           new object[] { propertySpec.MultiLineWidth }));
+                }
+
+                if (propertySpec.Choices != null)
+                {
+                    var choicesMethodBuilder = typeBuilder.DefineMethod(
+                        "Choices" + name, 
+                        MethodAttributes.Public,
+                        typeof (string[]), Type.EmptyTypes);
+
+                    iLGenerator = choicesMethodBuilder.GetILGenerator();
+
+                    iLGenerator.DeclareLocal(typeof (List<string>));
+                    iLGenerator.DeclareLocal(typeof (string[]));
+
+                    iLGenerator.Emit(OpCodes.Nop);
+                    iLGenerator.Emit(OpCodes.Ldc_I4, propertySpec.Choices.Count);
+                    iLGenerator.Emit(OpCodes.Newarr, typeof (string));
+                    iLGenerator.Emit(OpCodes.Stloc_1);
+
+                    var i = 0;
+                    foreach (var choice in propertySpec.Choices)
+                    {
+                        iLGenerator.Emit(OpCodes.Ldloc_1);
+                        iLGenerator.Emit(OpCodes.Ldc_I4, i++);
+                        iLGenerator.Emit(OpCodes.Ldstr, choice);
+                        iLGenerator.Emit(OpCodes.Stelem_Ref);
+                    }
+
+                    iLGenerator.Emit(OpCodes.Ldloc_1);
+
+                    var dvmToListMethod = typeof(DynamicViewModel).GetMethod("ToList", new Type[] { typeof(string[]) });
+                    iLGenerator.Emit(OpCodes.Call, dvmToListMethod);
+
+                    iLGenerator.Emit(OpCodes.Stloc_0);
+                    iLGenerator.Emit(OpCodes.Ldloc_0);
+                    iLGenerator.Emit(OpCodes.Ret);
+                }
+            }
+        }
+
+        public static List<string> ToList(params string[] strings)
+        {
+            return strings.ToList();
+        }
+    }
+}
